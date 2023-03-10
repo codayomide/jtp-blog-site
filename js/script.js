ec41383f55f9ce5b35dd95ea31127cc145e27f63
RalphFred <111363603+RalphFred@users.noreply.github.com>
2023-03-10 09:11:27 +0100

    worked on the major part of the hmtl and css

---
+++ js/script.js
@@ -0,0 +1,32 @@
+let left = document.getElementById('left');
+let right = document.getElementById('right');
+
+let blog = document.getElementsByClassName('blog');
+let blog_page = Math.ceil(blog.length/4);
+
+let l = 0;
+let movePer = 25;
+let maxMove = 150;
+
+let right_mover = ()=>{
+    l = l + movePer;
+    if (blog == 1){l = 0; }
+    for(const i of blog)
+    {
+        if (l > maxMove){l = l - movePer;}
+        i.style.left = '-' + l + '%';
+    }
+}
+
+let left_mover = ()=>{
+    l = l - movePer;
+    if (l<=0){l = 0;}
+    for(const i of blog){
+        if (blog_page>1){
+            i.style.left = '-' + l + '%';
+        }
+    }
+}
+
+right.onclick = ()=>{right_mover();}
+left.onclick = ()=>{left_mover();}
